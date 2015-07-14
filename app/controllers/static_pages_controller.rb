b5f3f678b377d99ddb061640abbf1565e10eea33
Tess Griffin <tessjgriffin@gmail.com>
2015-07-14 16:58:55 -0600

    Taking out the active query for events

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,9 @@ class StaticPagesController < ApplicationController
   end
 
   def index
-    @next_events = Event.active.order(:date).first(5)
-    @just_added = Event.active.order(:created_at).last(2)
+    @next_events = Event.order(:date).first(5)
+    # @next_events = Event.active.order(:date).first(5)
+    @just_added = Event.last(2)
+    # @just_added = Event.active.order(:created_at).last(2)
   end
 end
