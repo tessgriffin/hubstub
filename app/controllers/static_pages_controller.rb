6c5bf4656f9043c4ebd8e6e7a07a7e98695675d4
Tess Griffin <tessjgriffin@gmail.com>
2015-07-16 10:11:29 -0400

    Added paginiation to venues page. Removed paperclip settings from production environment config

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,8 +5,6 @@ class StaticPagesController < ApplicationController
 
   def index
     @next_events = Event.order(:date).first(5)
-    # @next_events = Event.active.order(:date).first(5)
     @just_added = Event.last(2)
-    # @just_added = Event.active.order(:created_at).last(2)
   end
 end
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,7 +1,9 @@
 class VenuesController < ApplicationController
   def show
     @venue = Venue.find_by(id: params[:id])
-    @items = Item.active.not_in_cart(session[:cart]).joins(:event).where(events: {venue_id: @venue.id})
-    @events = @items.map(&:event).uniq
+    @items = Item.active.not_in_cart(session[:cart])
+                        .joins(:event).where(events: {venue_id: @venue.id})
+                        .paginate(:page => params[:page], :per_page => 10)
+    @events = @venue.events.paginate(:page => params[:page], :per_page => 10)
   end
 end
